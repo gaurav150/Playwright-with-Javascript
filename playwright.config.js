92de790cdb39e152a1b788c1b5ea306ec4018660
KUMAG032 <gaurav.kumar.-nd3@disney.com>
2026-04-10 14:38:12 +0530

    updated playwright config file

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     timeout: 5000,
   },
   use: {
-    browserName: 'firefox',
+    browserName: 'chromium',
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
