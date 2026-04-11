f92c2266db2afa4304686f86ac5b22e9eef313de
KUMAG032 <gaurav.kumar.-nd3@disney.com>
2026-04-11 15:05:31 +0530

    section7 40 number

--- playwright.config.js
+++ playwright.config.js
@@ -38,6 +38,10 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: true,
+    viewport: null,
+  launchOptions: {
+    args: ['--start-maximized']
+  }
   },
   projects: [
     {
